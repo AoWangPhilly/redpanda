7651dbe4da70f1e46558bb07c0920a18768b0b4b
AoWangDrexel <aw3338@drexel.edu>
2020-08-28 17:57:30 -0400

    Add latex output and ignore image

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import os
+import sympy
 import discord
 from discord.ext import commands
-
+from sympy.parsing.sympy_parser import parse_expr
+import sympy
 bot = commands.Bot(command_prefix='>')
 
 
@@ -9,17 +11,25 @@ bot = commands.Bot(command_prefix='>')
 async def on_ready():
     print('Logged on as {0}!'.format(bot.user))
 
-@ bot.event 
-async def on_member_remove(member): 
-    print(f'{member} has left the server!! >:c') 
-    
-@bot.command() 
-async def ping(ctx): 
-    await ctx.send(f'pong! {round(bot.latency * 1000)}ms') 
 
-@bot.command() 
-async def solve(ctx, eq): 
-    await ctx.send('{}={}'.format(eq, eval(eq))) 
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} has left the server!! >:c')
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'pong! {round(bot.latency * 1000)}ms')
+
+
+@bot.command()
+async def integrate(ctx, *, eq):
+    try:
+        eq = parse_expr(eq.replace('^', '**'))
+        sympy.preview(sympy.integrate(eq), viewer='file', filename='output.png')
+        await ctx.send(file=discord.File('output.png'))
+    except:
+        await ctx.send('Error! Try again :<')
 
 if __name__ == '__main__':
     token = os.getenv('TOKEN')

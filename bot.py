9256fb60ecbfa7e82898d2c4cc16042372ec03d8
AoWangDrexel <aw3338@drexel.edu>
2020-08-28 16:39:19 -0400

    Test Discord bot and add in virtual env to gitignore

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import os
-
+import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='>')
@@ -9,6 +9,17 @@ bot = commands.Bot(command_prefix='>')
 async def on_ready():
     print('Logged on as {0}!'.format(bot.user))
 
+@ bot.event 
+async def on_member_remove(member): 
+    print(f'{member} has left the server!! >:c') 
+    
+@bot.command() 
+async def ping(ctx): 
+    await ctx.send(f'pong! {round(bot.latency * 1000)}ms') 
+
+@bot.command() 
+async def solve(ctx, eq): 
+    await ctx.send('{}={}'.format(eq, eval(eq))) 
 
 if __name__ == '__main__':
     token = os.getenv('TOKEN')

6d259d4f6ee17ab2b651b2103539d52ca55f7515
AoWangDrexel <aw3338@drexel.edu>
2020-08-29 21:01:57 -0400

    Add joke and hello methods

--- src/cogs/greetings.py
+++ src/cogs/greetings.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
+from random import choice
+import json
 
 
 class Greetings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self._last_member = None
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -12,17 +15,29 @@ class Greetings(commands.Cog):
         if channel is not None:
             await channel.send('Welcome {0.mention}!! >:D'.format(member))
 
+    @commands.command()
+    async def hello(self, ctx, *, member: discord.Member = None):
+        """Says hello"""
+        member = member or ctx.author
+        if self._last_member is None or self._last_member.id != member.id:
+            await ctx.send('Hello {0.name}~'.format(member))
+        else:
+            await ctx.send('Hello {0.name}... This feels familiar.'.format(member))
+        self._last_member = member
+
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         channel = member.guild.system_channel
         if channel is not None:
             await channel.send('{0.mention} had left the server!! >:c'.format(member))
-    
+
     @commands.command()
     async def joke(self, ctx):
-        pass
+        with open('jokes.json') as joke_file:
+            jokes = json.load(joke_file)
+            q = choice(list(jokes.keys()))
+            await ctx.send('{}\n{}'.format(q, jokes[q]))
 
     @commands.command()
     async def meme(self, ctx):
         pass
-

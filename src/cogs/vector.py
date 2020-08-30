0a8866b1fb20958ec111b157c6e0da706406d245
AoWangDrexel <aw3338@drexel.edu>
2020-08-30 17:08:04 -0400

    Add in norm and projections for vector

--- src/cogs/vector.py
+++ src/cogs/vector.py
@@ -3,25 +3,43 @@ from discord.ext import commands
 import sympy
 from sympy.matrices import Matrix
 from sympy.vector import CoordSys3D, matrix_to_vector
+  
 
 def convert(vec):
+    C = CoordSys3D('')
     vec = vec.replace('<', '').replace('>', '').split(',')
-    return Matrix(list(map(int, vec)))
+    return matrix_to_vector(Matrix(list(map(int, vec))), C)
+
 
 class Vector(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def dot(self, ctx, v1 : convert, v2 : convert):
+    async def dot(self, ctx, v1: convert, v2: convert):
         '''Computes dot product'''
         await ctx.send(v1.dot(v2))
 
     @commands.command()
-    async def cross(self, ctx, v1 : convert, v2 : convert):
+    async def cross(self, ctx, v1: convert, v2: convert):
         '''Computes cross product'''
-        C = CoordSys3D('')
-        cp = matrix_to_vector(v1.cross(v2), C)
-        sympy.preview(cp, viewer='file', filename='vector.png')
+        sympy.preview(v1.cross(v2), viewer='file', filename='vector.png')
         await ctx.send(file=discord.File('vector.png'))
 
+    @commands.command()
+    async def norm(self, ctx, vec: convert):
+        '''Computes norm of vector'''
+        await ctx.send(vec.magnitude())
+
+    @commands.command()
+    async def normalize(self, ctx, vec: convert):
+        '''Normalizes vector'''
+        sympy.preview(vec.normalize(), viewer='file', filename='vector.png')
+        await ctx.send(file=discord.File('vector.png'))
+
+    @commands.command()
+    async def proj(self, ctx, vec1: convert, vec2: convert):
+        '''Finds projection of first vector onto second'''
+        sympy.preview(vec1.projection(vec2),
+                      viewer='file', filename='vector.png')
+        await ctx.send(file=discord.File('vector.png'))

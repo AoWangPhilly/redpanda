489570cc3926afc68dda648b5deba177ef5e3e2e
AoWangDrexel <aw3338@drexel.edu>
2020-08-30 14:06:31 -0400

    Add in converters for Calculus

--- src/cogs/calculus.py
+++ src/cogs/calculus.py
@@ -4,35 +4,31 @@ import sympy
 from sympy.parsing.sympy_parser import parse_expr
 from sympy.plotting import plot
 
+def parseEq(eq):
+    eq = eq.replace('^', '**').replace('e', 'E')
+    return parse_expr(eq)
+
 class Calculus(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def derive(self, ctx, *, eq):
-        try:
-            eq = parse_expr(eq.replace('^', '**'))
-            sympy.preview(sympy.diff(eq), viewer='file',
-                        filename='output.png', dvioptions=['-D', '200'])
-            await ctx.send(file=discord.File('output.png'))
-        except:
-            await ctx.send('Error! Try again :<')
-            
+    async def derive(self, ctx, *, eq: parseEq):
+        '''Derives single variable equations'''
+        sympy.preview(sympy.diff(eq), viewer='file',
+                      filename='output.png', dvioptions=['-D', '200'])
+        await ctx.send(file=discord.File('output.png'))
 
     @commands.command()
-    async def integrate(self, ctx, *, eq):
-        try:
-            eq = parse_expr(eq.replace('^', '**'))
-            sympy.preview(sympy.integrate(eq), viewer='file',
-                        filename='output.png', dvioptions=['-D', '200'])
-            await ctx.send(file=discord.File('output.png'))
-        except:
-            await ctx.send('Error! Try again :<')
+    async def integrate(self, ctx, *, eq: parseEq):
+        '''Integrates single variable equations'''
+        sympy.preview(sympy.integrate(eq), viewer='file',
+                      filename='output.png', dvioptions=['-D', '200'])
+        await ctx.send(file=discord.File('output.png'))
 
     @commands.command()
-    async def graph(self, ctx, *, eq):
-        eq = parse_expr(eq.replace('^', '**'))
+    async def graph(self, ctx, *, eq: parseEq):
+        '''Graphs simple equations'''
         p1 = plot(eq, show=False)
         p1.save('graph.png')
         await ctx.send(file=discord.File('graph.png'))
-

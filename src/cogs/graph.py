a36d1d612af4bf7eac073c03c9494087fc0c9ece
Brian Chen <brianchen0810@hotmail.com>
2020-09-01 00:14:26 -0400

    Rename file location

--- src/cogs/graph.py
+++ src/cogs/graph.py
@@ -17,18 +17,19 @@ class Graph(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.file_location = 'temp/graph.png'
+        self.graph_location = 'temp/graph.png'
+        self.parametric_graph_location = 'temp/parametric_graph.png'
 
     @commands.command()
     async def graph(self, ctx, *, eq: parse_eq):
         """Graphs simple equations"""
         p1 = plot(eq, show=False)
-        p1.save(self.file_location)
-        await ctx.send(file=discord.File(self.file_location))
+        p1.save(self.graph_location)
+        await ctx.send(file=discord.File(self.graph_location))
 
     @commands.command(name='pgraph')
     async def p_graph(self, ctx, x: parse_eq, y: parse_eq):
         """Graph parametric equations"""
         p1 = plot_parametric(x, y)
-        p1.save(self.file_location)
-        await ctx.send(file=discord.File(self.file_location))
+        p1.save(self.graph_location)
+        await ctx.send(file=discord.File(self.parametric_graph_location))

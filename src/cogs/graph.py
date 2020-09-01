fdca24e064b1ae71a5929675fa48bf7312f16f5f
Brian Chen <brianchen0810@hotmail.com>
2020-09-01 00:12:34 -0400

    Add parametric graphing

--- src/cogs/graph.py
+++ src/cogs/graph.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from sympy import plot_parametric
 from sympy.parsing.sympy_parser import parse_expr
 from sympy.plotting import plot
 
@@ -22,5 +23,12 @@ class Graph(commands.Cog):
     async def graph(self, ctx, *, eq: parse_eq):
         """Graphs simple equations"""
         p1 = plot(eq, show=False)
+        p1.save(self.file_location)
+        await ctx.send(file=discord.File(self.file_location))
+
+    @commands.command(name='pgraph')
+    async def p_graph(self, ctx, x: parse_eq, y: parse_eq):
+        """Graph parametric equations"""
+        p1 = plot_parametric(x, y)
         p1.save(self.file_location)
         await ctx.send(file=discord.File(self.file_location))

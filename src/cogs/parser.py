810f6cd4f592d755fc4801e26729861d09944cde
AoWangDrexel <aw3338@drexel.edu>
2020-09-01 17:12:30 -0400

    Add in parser class for development

---
+++ src/cogs/parser.py
@@ -0,0 +1,12 @@
+class Parser():
+    def __init__(self, string):
+        self.string = string
+    
+    def equation(self):
+        pass
+
+    def point(self):
+        pass
+
+    def variables(self):
+        pass
